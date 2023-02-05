ef1afa4fbdc089b8026326822d6322d90d9c6412
NdekoCode <arickbulakali@gmail.com>
2023-02-05 12:58:44 +0200

    add dot button interaction

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <template>
   <div id="app" class="container mt-5">
-    <Carousel>
-      <CarouselSlide v-for="slide in nbrSlides" :key="slide" :index="slide - 1">
-        <h3 class="carousel__text">Salut les gens {{ slide }}</h3>
-        <img
-          width="940"
-          height="300"
-          :src="
-            require('@/assets/images/' +
-              slide.toString().padStart(2, '0') +
-              '.jpg')
-          "
-          alt=""
-        />
-      </CarouselSlide>
-    </Carousel>
-    <div><button @click="addSlide">Ajouter un slide</button></div>
+    <Carousel />
   </div>
 </template>
 
 <script>
 import Carousel from "./components/Carousel/Carousel";
-import CarouselSlide from "./components/Carousel/CarouselSlide";
 
 export default {
   name: "App",
-  data() {
-    return {
-      nbrSlides: 5,
-    };
-  },
-  methods: {
-    addSlide() {
-      if (this.nbrSlides < 10) {
-        this.nbrSlides += 1;
-      } else {
-        this.nbrSlides = 10;
-      }
-    },
-  },
   components: {
     Carousel,
-    CarouselSlide,
   },
 };
 </script>
--- src/components/Carousel/Carousel.vue
+++ src/components/Carousel/Carousel.vue
@@ -1,57 +1,76 @@
 <template>
-  <div class="carousel">
-    <slot></slot>
-    <button class="carousel__nav carousel__prev" @click.prevent="prev">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="ionicon"
-        heigth="24"
-        width="24"
-        viewBox="0 0 512 512"
-      >
-        <title>Chevron Back</title>
-        <path
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="48"
-          d="M328 112L184 256l144 144"
+  <div>
+    <div class="carousel">
+      <CarouselSlide v-for="slide in nbrSlides" :index="slide - 1" :key="slide">
+        <h3 class="carousel__text">Salut les gens {{ slide }}</h3>
+        <img
+          width="940"
+          height="300"
+          :src="
+            require('@/assets/images/' +
+              slide.toString().padStart(2, '0') +
+              '.jpg')
+          "
+          alt=""
         />
-      </svg>
-    </button>
-    <button class="carousel__nav carousel__next" @click.prevent="next">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="ionicon"
-        heigth="24"
-        width="24"
-        viewBox="0 0 512 512"
-      >
-        <title>Chevron Forward</title>
-        <path
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="48"
-          d="M184 112l144 144-144 144"
-        />
-      </svg>
-    </button>
-    <div class="carousel__pagination">
-      <button
-        class=""
-        v-for="item in slidesCount"
-        :key="item"
-        @click="goTo(item - 1)"
-        :class="{ active: item - 1 === index }"
-      ></button>
+      </CarouselSlide>
+      <button class="carousel__nav carousel__prev" @click.prevent="prev">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="ionicon"
+          heigth="24"
+          width="24"
+          viewBox="0 0 512 512"
+        >
+          <title>Chevron Back</title>
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="48"
+            d="M328 112L184 256l144 144"
+          />
+        </svg>
+      </button>
+      <button class="carousel__nav carousel__next" @click.prevent="next">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="ionicon"
+          heigth="24"
+          width="24"
+          viewBox="0 0 512 512"
+        >
+          <title>Chevron Forward</title>
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="48"
+            d="M184 112l144 144-144 144"
+          />
+        </svg>
+      </button>
+      <div class="carousel__pagination">
+        <button
+          class=""
+          v-for="item in slidesCount"
+          :key="item"
+          @click="goTo(item - 1)"
+          :class="{ active: item - 1 === index }"
+        ></button>
+      </div>
     </div>
+    <!-- <div class="carousel">
+      <button class="btn" @click.stop="addSlide">Ajouter un slide</button>
+      <button class="btn" @click.stop="removeSlide">Supprimer un slide</button>
+    </div> -->
   </div>
 </template>
 
 <script>
+import CarouselSlide from "./CarouselSlide";
 export default {
   name: "Carousel",
   data() {
@@ -59,6 +78,7 @@ export default {
       index: 0,
       slides: [],
       direction: "right",
+      nbrSlides: 5,
     };
   },
   methods: {
@@ -82,11 +102,25 @@ export default {
         this.index = this.slidesCount - 1;
       }
     },
+    addSlide() {
+      if (this.nbrSlides < 10) {
+        this.nbrSlides++;
+      }
+    },
+    /*  removeSlide() {
+      if (this.nbrSlides >= 1 && this.nbrSlides !== this.index + 1) {
+        this.nbrSlides -= 1;
+      }
+      this.$forceUpdate();
+    }, */
   },
   computed: {
     slidesCount() {
       return this.slides.length;
     },
+  },
+  components: {
+    CarouselSlide,
   },
   mounted() {
     this.slides = this.$children;
